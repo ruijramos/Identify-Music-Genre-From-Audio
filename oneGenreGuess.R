c6d4093957567b2fe4c55ad2b632e4fad3ff4a3a
Rui Jorge Ramos <rui.j.ramos@inesctec.pt>
2021-03-21 18:57:46 +0000

    + TESTS

--- oneGenreGuess.R
+++ oneGenreGuess.R
@@ -4,7 +4,7 @@
 dataset <- read.csv("data.csv", header=TRUE)
 
 # Import music values
-musicValues <- read.csv("testSongs/discoFromYoutube.csv", header=TRUE)
+musicValues <- read.csv("testSongs/metal3FromYoutube.csv", header=TRUE)
 
 # remove file name column
 dataset <- dataset[,-1]
