9c684d6238cf8b26e0f56d95597c4e1379fa200d
DuarteNRP <duarte_pereira_5@hotmail.com>
2021-03-24 19:24:52 +0000

    refactor code

--- Python_ExtractFeatures/createGlobalCsv.py
+++ Python_ExtractFeatures/createGlobalCsv.py
@@ -1,5 +1,5 @@
 # feature extractoring and preprocessing data
-import librosa
+import librosa 
 import pandas as pd
 import numpy as np
 import os
@@ -10,13 +10,13 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #csv header with file name and all attributes to classification
-header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+header = 'filename chroma_stft rms spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
 for i in range(1, 21):
     header += f' mfcc{i}'
 header += ' label'
 header = header.split()
 
-#write headers into csv
+#write header into csv
 file = open('data.csv', 'w', newline='')
 with file:
     writer = csv.writer(file)
@@ -29,15 +29,17 @@ genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split
 for g in genres:
     for filename in os.listdir(f'genres/{g}'):
         songname = f'genres/{g}/{filename}'
+        # Load the audio as a waveform `y`
+        # Store the sampling rate as `sr`
         y, sr = librosa.load(songname, mono=True, duration=30)
-        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-        rmse = librosa.feature.rms(y=y)
-        spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+        chroma_stft = librosa.feature.chroma_stft(y,sr)
+        rms = librosa.feature.rms(y)
+        spec_cent = librosa.feature.spectral_centroid(y, sr)
+        spec_bw = librosa.feature.spectral_bandwidth(y, sr)
+        rolloff = librosa.feature.spectral_rolloff(y, sr)
         zcr = librosa.feature.zero_crossing_rate(y)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr)
-        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+        mfcc = librosa.feature.mfcc(y,sr)
+        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rms)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
         for e in mfcc:
             to_append += f' {np.mean(e)}'
         to_append += f' {g}'
--- Python_ExtractFeatures/inputSong.py
+++ Python_ExtractFeatures/inputSong.py
@@ -14,13 +14,13 @@ warnings.filterwarnings('ignore')
 filename = sys.argv[-1]
 
 #csv header with file name and all attributes to classification
-header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+header = 'filename chroma_stft rms spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
 for i in range(1, 21):
     header += f' mfcc{i}'
 header = header.split()
 
 #write headers into csv
-file = open('testMySong.csv', 'w', newline='')
+file = open('testMySong1.csv', 'w', newline='')
 with file:
     writer = csv.writer(file)
     writer.writerow(header)
@@ -28,23 +28,25 @@ with file:
 #extract all features from spectogram 
 songname = f'{filename}'
 y, sr = librosa.load(songname, mono=True, duration=30)
-chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-rmse = librosa.feature.rms(y=y)
-spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+# Load the audio as a waveform `y`
+# Store the sampling rate as `sr`
+chroma_stft = librosa.feature.chroma_stft(y,sr)
+rms = librosa.feature.rms(y)
+spec_cent = librosa.feature.spectral_centroid(y,sr)
+spec_bw = librosa.feature.spectral_bandwidth(y,sr)
+rolloff = librosa.feature.spectral_rolloff(y,sr)
 zcr = librosa.feature.zero_crossing_rate(y)
-mfcc = librosa.feature.mfcc(y=y, sr=sr)
-to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+mfcc = librosa.feature.mfcc(y, sr)
+to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rms)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
 for e in mfcc:
 	to_append += f' {np.mean(e)}'
 
 #write in file
-file = open('testMySong.csv', 'a', newline='')
+file = open('testMySong1.csv', 'a', newline='')
 with file:
 	writer = csv.writer(file)
 	writer.writerow(to_append.split())
 
 #read and filter data
-data = pd.read_csv('testMySong.csv')
+data = pd.read_csv('testMySong1.csv')
 print(data)		
